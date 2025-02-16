677f73ee431c9376201f215b318a7bd858d57bf1
sneha <snehamittal88293@gmail.com>
2025-02-16 21:42:02 +0530

    todo schema

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+//mongodb+srv://sneha:4vEx56mTyx9W4Xp5@cluster0.cywi7.mongodb.net/todo2
+
+  const mongoose = require("mongoose");
+
+  mongoose.connect("mongodb+srv://sneha:4vEx56mTyx9W4Xp5@cluster0.cywi7.mongodb.net/todo2")
+  const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+  })
+
+  const todo = mongoose.model('todos', todoSchema);
+  module.exports= {
+    todo
+  }
--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,34 @@
 //write basic express boilerplate code
 // with express.json() middleware
 
-const express= require("express")
+const express= require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", function(req, res){
-
+      const createPayLoad = req.body;
+      const parsePayLoad = createTodo.safeParse(createPayLoad);
+      if(!parsePayLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong input",
+        })
+        return;
+      }
 })
 
 app.get("/todos", function(req, res){
-    
+
 })
 
 app.put("/completed", function(req, res){
-
+  const updatePayLoad = req.body;
+  const parsePayLoad = updateTodo.safeParse(updatePayLoad);
+  if(!parsePayLoad.success){
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    })
+    return;
+  }
 })
